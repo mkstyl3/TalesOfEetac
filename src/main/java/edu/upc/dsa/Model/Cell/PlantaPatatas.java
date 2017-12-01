29d6bb1cc95a69e3440c910b8efdd050a55d4c21
Dani <dani.dalfo.f@gmail.com>
2017-12-01 13:21:35 +0100

    noves ideas

---
+++ src/main/java/edu/upc/dsa/Model/Cell/PlantaPatatas.java
@@ -0,0 +1,13 @@
+package edu.upc.dsa.Model.Cell;
+
+public class PlantaPatatas extends Cell {
+
+    final protected String symbol = "P";
+
+    public PlantaPatatas () {}
+
+    @Override
+    public String getSymbol() {
+        return this.symbol;
+    }
+}
